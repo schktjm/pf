0a13a01157c780720d981b007573596eff9e95cb
schktjm <si2.o.mjt@gmail.com>
2021-05-01 15:43:58 +0900

    :tada: init react

---
+++ src/page/Home.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import styled from 'styled-components'
+
+type Props = {};
+
+const Wrapper = styled.div``;
+
+const Home: React.FC<Props> = () => (
+    <Wrapper>hello</Wrapper>
+);
+
+export default Home;
