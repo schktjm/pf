daf324bc52748aad394fbd4ba915c19df32cd95c
schktjm <si2.o.mjt@gmail.com>
2021-07-04 20:42:49 +0900

    :+1: button animation

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -81,28 +81,64 @@ const Text = styled.p`
     font-weight: normal;
     font-size: 0.75em;
 `;
-const IconButton = styled.button`
+const IconLink = styled.a`
     margin-right: 30px;
-    color: #E0E0E0;
+    color: #F5F5F5;
     border: none;
     background: none;
     padding: 0;
-    display: inline;
+    display: inline-block;
     font-size: inherit;
+    transition: all 300ms ease-in-out;
+    position: relative;
 
     &:last-child{
         margin-right: 0px;
     }
 
     &:visited {
-        color: #E0E0E0;
+        color: #F5F5F5;
     }
 
     &:hover {
-        color: #616161;
+        transform: translateY(-0.25em);
+        cursor: pointer;
     }
 `;
-const IconLink = IconButton.withComponent('a')
+const IconBtn = IconLink.withComponent('button')
+const IconButton = styled(IconBtn)`
+    &:active {
+        & > span {
+            visibility: visible;
+        }
+    }
+`
+const Tooltip = styled.span`
+    transition: visibility 1s ease-out;
+    visibility: hidden;
+    z-index: 1;
+    background: #E0E0E0;
+    color: black;
+    position: absolute;
+    top: -60%;
+    left: -40%;
+    border-radius: 4px;
+    font-size: 0.4em;
+    padding: 2px 4px;
+    
+    &::after {
+        content: "";
+        width: 0;
+        height: 0;
+        border-style: solid;
+        border-width: 5px 5px 0 5px;
+        border-color: #E0E0E0 transparent transparent transparent;
+        position: absolute;
+        left: calc(50% - 5px);
+        top: 100%;
+    }
+
+`
 
 const Home: React.FC<Props> = () => {
     const copyMail = () => {
@@ -125,6 +161,7 @@ const Home: React.FC<Props> = () => {
                         <FontAwesomeIcon icon={['fas', 'blog']} />
                     </IconLink>
                     <IconButton type="button" onClick={copyMail}>
+                        <Tooltip>Copyed!</Tooltip>
                         <FontAwesomeIcon icon={['fas', 'envelope']} />
                     </IconButton>
                 </nav>
